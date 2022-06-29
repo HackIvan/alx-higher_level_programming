01a801b0fc0660315832e7e690a9ec4586bfae00
Ivan <ibrahimivank@hotmail.com>
2022-06-29 03:35:34 -0700

    Final commit 0x08-pythone-more_classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This Program Create an Empty Rectangle Class"""
+
+
+class Rectangle():
+    """Do Nothing"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This the official readme file for this project 0x08-pythone-more_classes
